69f2b27379a6f6d14f112620d49acdbe8f184cfe
NaskoksaN <nbobchev@yahoo.com>
2024-02-28 16:23:44 +0200

    Initial project frame

--- TruckManagementWeb/Program.cs
+++ TruckManagementWeb/Program.cs
@@ -1,7 +1,3 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using TruckManagementWeb.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
