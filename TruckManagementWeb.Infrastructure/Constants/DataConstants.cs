409314692fc973daf9e4054bcbf12ad91f0c7323
NaskoksaN <nbobchev@yahoo.com>
2024-02-29 15:21:30 +0200

    Add Truck Tabel

---
+++ TruckManagementWeb.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace TruckManagementWeb.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int TruckPlateMinLength = 6;
+        public const int TruckPlateMaxLength = 10;
+
+        public const int TruckBrandMinLength = 4;
+        public const int TruckBrandMaxLength = 40;
+
+        public const int TruckModelMinLength = 2;
+        public const int TruckModelMaxLength = 30;
+    }
+}
---
+++ TruckManagementWeb.Infrastructure/Data/Models/Truck.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+using static TruckManagementWeb.Infrastructure.Constants.DataConstants;
+
+namespace TruckManagementWeb.Infrastructure.Data.Models
+{
+    [Comment("Truck tabel")]
+    public class Truck
+    {
+        /// <summary>
+        /// Uunique identifier of the truck.
+        /// </summary>
+        [Key]
+        [Comment("Truck identifier")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The license plate number of the truck.
+        /// </summary>
+        [Required]
+        [Comment("Truck plate")]
+        [MaxLength(TruckPlateMaxLength)]
+        public string TruckPlate { get; set; } = string.Empty;
+
+        /// <summary>
+        /// the brand of the truck.
+        /// </summary>
+        [Required]
+        [Comment("Truck brand")]
+        [MaxLength(TruckBrandMaxLength)]
+        public string TruckBrand { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Model of the truck.
+        /// </summary>
+        [Required]
+        [Comment("Truck model")]
+        [MaxLength(TruckModelMaxLength)]
+        public string TruckModel { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Production year of the truck.
+        /// </summary>
+        [Required]
+        [Comment("Truck production year")]
+        public int ProductionYear { get; set; }
+        /// <summary>
+        /// Indicating whether the truck is active or removed from service
+        /// </summary>
+        [Required]
+        [Comment("Truck activity")]
+        public bool IsActive { get; set; } = true;
+    }
+}
