2528c7be84dcee2c5f1a8136303c289ce700f5df
NaskoksaN <nbobchev@yahoo.com>
2024-03-02 17:40:03 +0200

    Create modelView for TruckController

---
+++ TruckManagementWeb.Core/Models/Truck/TruckFormModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TruckManagementWeb.Core.Constants.MessageConstants;
+using static TruckManagementWeb.Core.Constants.ValidationConstants;
+using static TruckManagementWeb.Infrastructure.Constants.DataConstants;
+
+namespace TruckManagementWeb.Core.Models.Truck
+{
+    public class TruckFormModel
+    {
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Truck plate")]
+        [StringLength(TruckPlateMaxLength,
+            MinimumLength = TruckBrandMinLength,
+            ErrorMessage = LengthMessage)]
+        [RegularExpression(PlateRegexValidation,
+            ErrorMessage = PlateMessage)]
+        public string TruckPlate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Truck brand")]
+        [StringLength(TruckBrandMaxLength,
+            MinimumLength = TruckBrandMinLength,
+            ErrorMessage = LengthMessage)]
+        public string TruckBrand { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Truck model")]
+        [StringLength(TruckModelMaxLength,
+            MinimumLength = TruckModelMinLength,
+            ErrorMessage = LengthMessage)]
+        public string TruckModel { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "year of production")]
+        [Range(TruckProductionYearMinYear, 
+            TruckProductionYearMaxYear,
+            ErrorMessage = ProductionRangeMessage)]
+        public int ProductionYear { get; set; } = DateTime.Now.Year;
+    }
+}
---
+++ TruckManagementWeb.Core/Models/Truck/TruckViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TruckManagementWeb.Core.Models.Truck
+{
+    public class TruckViewModel
+    {
+        public int Id { get; set; }
+        public string TruckPlate { get; set; } = string.Empty;
+        public string TruckBrand { get; set; } = string.Empty;
+        public string TruckModel { get; set; } = string.Empty;
+        public int ProductionYear { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
