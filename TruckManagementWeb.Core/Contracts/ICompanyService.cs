b7f8c6b92b789688614e757950e1fd50fd90922a
NaskoksaN <nbobchev@yahoo.com>
2024-03-03 16:52:29 +0200

    Added IcompanyService and CompanyServic methods

--- TruckManagementWeb.Core/Contracts/ICompanyService.cs
+++ TruckManagementWeb.Core/Contracts/ICompanyService.cs
@@ -10,7 +10,11 @@ namespace TruckManagementWeb.Core.Contracts
     public interface ICompanyService
     {
         Task<int> CreateAsync(CompanyFormModel form);
+        Task EditAsync(int id, CompanyEditFormModel form);
         Task<CompanyViewModel?> FindCompanyByIdAsync(int id);
+        Task<CompanyViewModel> FindCompanyByVatAsync(string companyVat);
+        Task<CompanyEditFormModel> GetCompanyForEditByIdAsync(int id);
         Task<bool> IsCompanyExistByVat(string companyVat);
+        Task<CompanyViewModel> RemoveCompanyByIdAsync(int id);
     }
 }
--- TruckManagementWeb.Core/Service/CompanyService.cs
+++ TruckManagementWeb.Core/Service/CompanyService.cs
@@ -33,6 +33,19 @@ namespace TruckManagementWeb.Core.Service
             return company.Id;
         }
 
+        public async Task EditAsync(int id, CompanyEditFormModel form)
+        {
+            Company company = await repository.GetByIdAsync<Company>(id);
+
+            company.CompanyVat = form.CompanyVat.ToUpper();
+            company.CompanyAddress = form.Address;
+            company.CompanyName = form.Name;
+            company.CompanyTown = form.Town;
+            company.CompanyCountry = form.Country;
+
+            await repository.SaveChangesAsync();
+        }
+
         public async Task<CompanyViewModel?> FindCompanyByIdAsync(int id)
         {
             CompanyViewModel? model = await repository.AllAsync<Company>()
@@ -53,8 +66,52 @@ namespace TruckManagementWeb.Core.Service
             return model;
         }
 
+        public async Task<CompanyViewModel?> FindCompanyByVatAsync(string companyVat)
+        {
+            return await repository.AllAsync<Company>()
+                .Where(c => c.CompanyVat.ToUpper() == companyVat.ToUpper())
+                .Select(c => new CompanyViewModel()
+                {
+                    Id = c.Id,
+                    Active = c.IsActive,
+                    Name = c.CompanyName,
+                    Vat = c.CompanyVat,
+                    Country = c.CompanyCountry,
+                })
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<CompanyEditFormModel?> GetCompanyForEditByIdAsync(int id)
+        {
+            CompanyEditFormModel? form = await repository.AllAsync<Company>()
+                .Where(c => c.Id == id)
+                .Select(c => new CompanyEditFormModel()
+                {
+                    Id=c.Id,
+                    Name = c.CompanyName,
+                    CompanyVat = c.CompanyVat.ToUpper(),
+                    Country = c.CompanyCountry,
+                    Town = c.CompanyTown,
+                    Address = c.CompanyAddress
+                })
+                .FirstOrDefaultAsync();
+
+            return form;
+        }
+
         public async Task<bool> IsCompanyExistByVat(string companyVat)
         => await repository.AllReadOnlyAsync<Company>()
             .AnyAsync(c=> c.CompanyVat == companyVat.ToUpper());
+
+        public async Task<CompanyViewModel?> RemoveCompanyByIdAsync(int id)
+        {
+            Company company = await repository.GetByIdAsync<Company>(id);
+            company.IsActive = false;
+            company.RemovedDate=DateTime.UtcNow.Date;
+
+            await repository.SaveChangesAsync();
+
+            return await FindCompanyByIdAsync(id);
+        }
     }
 }
