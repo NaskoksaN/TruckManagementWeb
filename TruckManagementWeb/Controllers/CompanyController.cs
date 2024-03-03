d9e2e89f3a1fbe91c8c591289ada0a61dd279df0
NaskoksaN <nbobchev@yahoo.com>
2024-03-03 14:55:05 +0200

    Made AddCompany in CompanyController

--- TruckManagementWeb/Controllers/CompanyController.cs
+++ TruckManagementWeb/Controllers/CompanyController.cs
@@ -1,11 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TruckManagementWeb.Core.Contracts;
+using TruckManagementWeb.Core.Models.Company;
 
 namespace TruckManagementWeb.Controllers
 {
     public class CompanyController : Controller
     {
-        public IActionResult Index()
+        private readonly ICompanyService service;
+        private readonly ILogger<HomeController> logger;
+        public CompanyController(ICompanyService _service,
+                        ILogger<HomeController> _logger)
         {
+            service = _service;
+            logger = _logger;
+        }
+        [HttpGet]
+        public IActionResult AddCompany()
+        {
+            CompanyFormModel form = new();
+
+            return View(form);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCompany(CompanyFormModel form)
+        {
+            if (await service.IsCompanyExistByVat(form.CompanyVat))
+            {
+                this.ModelState.AddModelError(nameof(form.CompanyVat),
+                   "Truck with this plate already added");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View(form);
+            }
+
+            int newCompanyId = await service.CreateAsync(form);
+
+            return RedirectToAction(nameof(CompanyController.CompanyDetails), new { id = newCompanyId });
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> CompanyDetails(int id)
+        {
+            CompanyViewModel? viewModel = await service.FindCompanyByIdAsync(id);
+
             return View();
         }
     }
