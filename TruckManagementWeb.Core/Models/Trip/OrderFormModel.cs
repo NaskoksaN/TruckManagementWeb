58aabd5d689ba25bfc316b1c5af2d2c777e9422d
NaskoksaN <nbobchev@yahoo.com>
2024-03-05 18:22:53 +0200

    Create TripForm, OrderForm

---
+++ TruckManagementWeb.Core/Models/Trip/OrderFormModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TruckManagementWeb.Core.Constants.MessageConstants;
+using static TruckManagementWeb.Infrastructure.Constants.DataConstants;
+
+namespace TruckManagementWeb.Core.Models.Trip
+{
+    public class OrderFormModel
+    {
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Order price")]
+        [Range(typeof(Decimal),
+            OrderMinPrice,
+            orderMaxPrice,
+            ErrorMessage = TripPriceMessage)]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        public DateTime LoadingDate { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        public DateTime DeliveryDate { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PostCodeMaxLength,
+            MinimumLength = PostCodeMinLength,
+            ErrorMessage = LengthMessage)]
+        public string LoadingPostCode { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [StringLength(PostCodeMaxLength,
+            MinimumLength = PostCodeMinLength,
+            ErrorMessage = LengthMessage)]
+        public string DeliveryPostCode { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public int TripId { get; set; }
+    }
+}
---
+++ TruckManagementWeb.Core/Models/Trip/TripFormModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static TruckManagementWeb.Core.Constants.MessageConstants;
+using static TruckManagementWeb.Core.Constants.ValidationConstants;
+using static TruckManagementWeb.Infrastructure.Constants.DataConstants;
+
+namespace TruckManagementWeb.Core.Models.Trip
+{
+    public class TripFormModel
+    {
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Truck plate")]
+        [StringLength(TruckPlateMaxLength,
+            MinimumLength = TruckBrandMinLength,
+            ErrorMessage = LengthMessage)]
+        [RegularExpression(PlateRegexValidation,
+            ErrorMessage = PlateMessage)]
+        public string TruckPlate { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Trip total km")]
+        [Range(TripMinKm, TripMaxKm, ErrorMessage = MileageMessage)]
+        public int TripKm { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Trip total price")]
+        [Range(typeof(Decimal), 
+            TripMinPrice, 
+            TripMaxPrice,
+            ErrorMessage = TripPriceMessage)]
+        public decimal TripPrice { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Trip start date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = RequiredMessage)]
+        [Display(Name = "Trip end date")]
+        public DateTime EndDate { get; set; }
+
+        ICollection<OrderFormModel> Orders { get; set; } = new List<OrderFormModel>();
+    }
+}
