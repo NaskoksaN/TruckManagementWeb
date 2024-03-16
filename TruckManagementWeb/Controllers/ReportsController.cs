e0db037e5e9dea4dfabba77cf63b64827e30a7ff
NaskoksaN <nbobchev@yahoo.com>
2024-03-16 18:00:02 +0200

    Add Reports in Controller, View . Make partial view

---
+++ TruckManagementWeb/Controllers/ReportsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruckManagementWeb.Core.Contracts;
+using TruckManagementWeb.Core.Models.Reports;
+using TruckManagementWeb.Core.Models.Truck;
+
+namespace TruckManagementWeb.Controllers
+{
+    public class ReportsController : Controller
+    {
+        private readonly ITruckService truckService;
+        private readonly IReports service;
+        public ReportsController(
+            ITruckService _truckService,
+            IReports _service)
+        {
+            truckService = _truckService;
+            service = _service;
+        }
+        [HttpGet]
+        public IActionResult TruckReportLastMonth()
+        {
+            FindTruckFormModel formModel = new();
+
+            return View("FindTruck", formModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> TruckReportLastMonth(FindTruckFormModel formModel)
+        {
+            var truck = await truckService.FindTruckByPlateAsync(formModel.TruckPlate);
+            if(truck == null)
+            {
+                this.ModelState.AddModelError(nameof(formModel.TruckPlate),
+                    "Truck with this plate was not found");
+                return View("FindTruck", formModel);
+            }
+
+            TruckMonthReportsViewModel truckReport = await service
+                        .GetTruckAndLastMonthResultAsync(formModel.TruckPlate);
+            
+            
+            return View(truckReport);
+        }
+    }
+}
--- TruckManagementWeb/Extensions/ServiceCollectionExtension.cs
+++ TruckManagementWeb/Extensions/ServiceCollectionExtension.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddScoped<ITruckService, TruckService>();
             services.AddScoped<ICompanyService, CompanyService>();
             services.AddScoped<ITripService, TripService>();
+            services.AddScoped<IReports, ReportService>();
 
             return services;
         }
