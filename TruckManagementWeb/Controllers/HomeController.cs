50beda9fb45963a6a0ec1104a8603bde7c76fd0d
NaskoksaN <nbobchev@yahoo.com>
2024-03-24 13:19:20 +0200

    add error action

--- TruckManagementWeb/Controllers/HomeController.cs
+++ TruckManagementWeb/Controllers/HomeController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TruckManagementWeb.Models;
 
 namespace TruckManagementWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -12,16 +13,25 @@ namespace TruckManagementWeb.Controllers
         {
             _logger = logger;
         }
-
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public async Task<IActionResult> Error(int statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (statusCode == 400)
+            {
+                return View("Error400");
+            }
+            if (statusCode == 401)
+            {
+                return View("Error401");
+            }
+            return View();
         }
     }
 }
