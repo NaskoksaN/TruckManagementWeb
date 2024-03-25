a66afb4d242e6253e6f89573978227425558149c
NaskoksaN <nbobchev@yahoo.com>
2024-03-25 20:44:22 +0200

    Add View admin with registration

---
+++ TruckManagementWeb.Core/Models/Admin/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TruckManagementWeb.Core.Models.Admin
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
