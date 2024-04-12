c7c094f746bff24eafc3c371bc219dec9e60b770
NaskoksaN <nbobchev@yahoo.com>
2024-04-12 19:05:14 +0300

    Add custom Validation for DateTime

---
+++ TruckManagementWeb.Core/Validations/DateRangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruckManagementWeb.Core.Validations
+{
+    public class DateRangeAttribute : ValidationAttribute
+    {
+        private readonly int _maxDaysDifference;
+
+        public DateRangeAttribute(int maxDaysDifference)
+        {
+            _maxDaysDifference = maxDaysDifference;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var endDate = (DateTime)value;
+            var startDateProperty = validationContext.ObjectType.GetProperty("StartDate");
+            var startDate = (DateTime)startDateProperty.GetValue(validationContext.ObjectInstance, null);
+
+            var differenceInDays = (endDate - startDate).Days;
+
+            if (differenceInDays > _maxDaysDifference)
+            {
+                return new ValidationResult($"The difference between Start Date and End Date cannot exceed {_maxDaysDifference} days.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
