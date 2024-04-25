963a3606121e779c90bb7a2f1dfcc42a2b065a1b
NaskoksaN <nbobchev@yahoo.com>
2024-04-25 17:45:39 +0300

    Add mailSubject and mailContetn

--- TruckManagementWeb.Core/Service/MyEmailService.cs
+++ TruckManagementWeb.Core/Service/MyEmailService.cs
@@ -5,19 +5,19 @@ namespace TruckManagementWeb.Core.Service
 {
     public class MyEmailService : IMyEmailService
     {
-        public void SendMail(string Email, string body, string fullName)
+        public void SendMail(string Email, string body, string mailSubject)
         {
             var email = new MimeMessage();
             email.From.Add(MailboxAddress.Parse("webtruckfounder@abv.bg"));
             email.To.Add(MailboxAddress.Parse(Email));
-            email.Subject = $"{fullName}, your work account is created, find login details";
+            email.Subject = $"{mailSubject}";
             email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
 
             try
             {
                 using (var smtp = new MailKit.Net.Smtp.SmtpClient())
                 {
-                    smtp.Connect("smtp.abv.bg", 465, true); // Use SSL/TLS
+                    smtp.Connect("smtp.abv.bg", 465, true); 
                     smtp.Authenticate("webtruckfounder@abv.bg", "Aa@123456");
                     smtp.Send(email);
                     smtp.Disconnect(true);
