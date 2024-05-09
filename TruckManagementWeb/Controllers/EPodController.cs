c6edff51f7c219e28144f56bc16af64af50c0b78
NaskoksaN <nbobchev@yahoo.com>
2024-05-09 15:41:07 +0300

    Rename Link Controller with new name EPod

---
+++ TruckManagementWeb/Controllers/EPodController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TruckManagementWeb.Controllers
+{
+    public class EPodController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index(Guid token)
+        {
+            return RedirectToAction("ConfirmOrder", "SellOrder", new { area = "Spedition", token });
+        }
+    }
+}
